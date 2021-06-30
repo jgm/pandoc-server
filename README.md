dfaa7088a2071b63c4805751af6337a7b0516905
John MacFarlane <jgm@berkeley.edu>
2021-06-30 09:17:44 -0700

    Add some options and a README.

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # pandoc-server
+
+A small example of a servant-based server for doing pandoc conversions.
+Customize to fit your needs!
+
+To install:
+
+```
+% stack install
+```
+
+To run the server:
+
+```
+% pandoc-server
+```
+
+This makes the server available at localhost port 8080.
+
+There is one endpoint, `/convert`.  It expects a GET request
+with a JSON request body containing the following fields:
+`text` (text to convert),
+`from` (defaults to `"markdown"`),
+`to` (defaults to `"html"`),
+`wrapText` (`"WrapAuto"` (default), `"WrapNone"`, or `"WrapPreserve"`),
+`columns` (integer, defaults to 72).
+
+Example of use (with `httpie`):
+
+```
+% http GET http://localhost:8080/convert text=:+1: from=markdown+emoji to=mediawiki Accept:text/plain
+HTTP/1.1 200 OK
+Content-Type: text/plain;charset=utf-8
+Date: Wed, 30 Jun 2021 15:51:15 GMT
+Server: Warp/3.3.16
+Transfer-Encoding: chunked
+
+<span class="emoji" data-emoji="+1">👍</span>
+```
+
--- src/Lib.hs
+++ src/Lib.hs
@@ -17,22 +17,32 @@ import Text.Pandoc
 import Data.Text (Text)
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Encoding as TLE
-import Control.Monad.IO.Class (liftIO)
+import Data.Maybe (fromMaybe)
 
+-- This is the data to be supplied by the JSON payload
+-- of requests.  Maybe values may be omitted and will be
+-- given default values.
 data Params = Params
-  { from :: Text
-  , to   :: Text
-  , text :: Text
-  } deriving (Eq, Show)
+  { text           :: Text
+  , from           :: Maybe Text
+  , to             :: Maybe Text
+  , wrapText       :: Maybe WrapOption
+  , columns        :: Maybe Int
+  } deriving (Show)
 
+-- Automatically derive code to convert to/from JSON.
 $(deriveJSON defaultOptions ''Params)
 
-type API = "convert"
-           :> ReqBody '[JSON] Params
-           :> Get '[PlainText, JSON] Text
+-- This is the API.  The "/convert" endpoint takes a request body
+-- consisting of a JSON-encoded Params structure and responds to
+-- Get requests with either plain text or JSON, depending on the
+-- Accept header.
+type API = "convert" :> ReqBody '[JSON] Params :> Get '[PlainText, JSON] Text
 
 startApp :: IO ()
-startApp = run 8080 app
+startApp = do
+  putStrLn "Starting server on port 8080."
+  run 8080 app
 
 app :: Application
 app = serve api server
@@ -43,18 +53,31 @@ api = Proxy
 server :: Server API
 server = convert
  where
-  convert params = handleErr =<< (liftIO . runIO $ do
-      (reader, readerExts) <- getReader (from params)
-      (writer, writerExts) <- getWriter (to params)
-      case (reader, writer) of
-        (TextReader r , TextWriter w) ->
-          r def{ readerExtensions = readerExts } (text params) >>=
-             w def{ writerExtensions = writerExts }
-        (TextReader _, _) -> throwError $ PandocAppError $
-                              (to params) <> " is not a text writer"
-        (_, _) -> throwError $ PandocAppError $
-                              (from params) <> " is not a text reader")
+  -- We use runPure for the pandoc conversions, which ensures that
+  -- they will do no IO.  This makes the server safe to use.  However,
+  -- it will mean that features requiring IO, like RST includes, will not work.
+  -- Changing this to
+  --    handleErr =<< liftIO (runIO (convert' params))
+  -- will allow the IO operations.
+  convert params = handleErr $ runPure (convert' params)
+
+  convert' :: PandocMonad m => Params -> m Text
+  convert' params = do
+    let readerFormat = fromMaybe "markdown" $ from params
+    let writerFormat = fromMaybe "html" $ to params
+    (reader, readerExts) <- getReader readerFormat
+    (writer, writerExts) <- getWriter writerFormat
+    case (reader, writer) of
+      (TextReader r , TextWriter w) ->
+        r def{ readerExtensions = readerExts } (text params) >>=
+           w def{ writerExtensions = writerExts
+                , writerWrapText = fromMaybe WrapAuto (wrapText params)
+                , writerColumns = fromMaybe 72 (columns params) }
+      (TextReader _, _) -> throwError $ PandocAppError $
+                            writerFormat <> " is not a text writer"
+      (_, _) -> throwError $ PandocAppError $
+                            readerFormat <> " is not a text reader"
+
   handleErr (Right t) = return t
-  handleErr (Left err) =
-    throwError $ err500 { errBody = TLE.encodeUtf8 $ TL.fromStrict $
-                                   renderError err }
+  handleErr (Left err) = throwError $
+    err500 { errBody = TLE.encodeUtf8 $ TL.fromStrict $ renderError err }
